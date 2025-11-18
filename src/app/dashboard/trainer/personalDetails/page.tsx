5c189d574e7c6ba6a11cfa9fa89ee830eac520b0
SARITsarit <876sarit@gmail.com>
2025-11-18 14:40:24 +0200

    update

--- src/app/dashboard/trainer/personalDetails/page.tsx
+++ src/app/dashboard/trainer/personalDetails/page.tsx
@@ -1,7 +1,67 @@
+"use client";
+
+import React from "react";
+import UniversalHeader from "@/components/header/header";
+import styles from "./style.module.css"; // שינוי כאן
+
 export default function PersonalDetailsPage() {
-  return (
-    <div>
-      <h1>Personal Details Page</h1>
+  return ( 
+    <div className={styles.pdContainer}>
+
+      <UniversalHeader role="trainer" />
+
+      {/* ------ FILTERS ------ */}
+      <div className={styles.pdFilters}>
+        <div className={styles.pdFilterBox}>סוג אימון:</div>
+        <div className={styles.pdFilterBox}>מקום אימון:</div>
+      </div>
+
+      {/* ------ TABLE ------ */}
+      <table className={styles.pdTable}>
+        <thead>
+          <tr>
+            <th>ראשון</th>
+            <th>שני</th>
+            <th>שלישי</th>
+            <th>רביעי</th>
+            <th>חמישי</th>
+            <th>שישי</th>
+            <th>שבת</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            {/* ראשון */}
+            <td className={styles.pdCell}>
+              <div className={styles.pdTrainingBox}>
+                <strong>1</strong>
+                <p>משעה:</p>
+                <p>עד שעה:</p>
+                <p>אימון קבוצתי</p>
+                <p>(סוג אימון: )</p>
+                <p>אימון אישי</p>
+              </div>
+            </td>
+
+            {/* שאר הימים */}
+            <td className={styles.pdCell}></td>
+            <td className={styles.pdCell}></td>
+            <td className={styles.pdCell}></td>
+            <td className={styles.pdCell}></td>
+            <td className={styles.pdCell}></td>
+            <td className={styles.pdCell}></td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* ------ ADD TRAINING ------ */}
+      <button className={styles.pdAddBtn}>הוספת אימון</button>
+
+      {/* ------ SAVE ------ */}
+      <div className={styles.pdSaveWrapper}>
+        <button className={styles.pdSaveBtn}>שמירת שינויים</button>
+      </div>
     </div>
   );
 }
--- src/lib/lang.ts
+++ src/lib/lang.ts
@@ -6,3 +6,13 @@ export const reviews = [
     { author: "עומר גולן", text: "אני נהנה לקרוא כל טיפ חדש, תודה!" },
     { author: "תמר רוזן", text: "איזה אנרגיות! האתר הזה פשוט נותן כוח." },
 ];
+
+ export const blocks=[{ type: "image", src: "/images/gym8.png" },
+          { type: "text", text: "הצלחה מתחילה בהחלטה." },
+          { type: "image", src: "/images/gym2.png" },
+          { type: "text", text: "אין קיצורי דרך — יש התמדה." },
+          { type: "image", src: "/images/gym6.png" },
+          { type: "text", text: "כל יום טוב יותר מאתמול." },
+          { type: "image", src: "/images/gym2.png" },
+          { type: "text", text: "הגוף שלך שומע כל מחשבה שלך." },
+          { type: "image", src: "/images/gym7.png" }]
