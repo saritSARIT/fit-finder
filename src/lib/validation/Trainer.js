0438c8343e3b74da8c3efdc95325b214d6238b5a
Tamar <tamar75262@gmail.com>
2025-11-26 15:35:59 +0200

    trainer session

--- src/lib/validation/Trainer.js
+++ src/lib/validation/Trainer.js
@@ -10,7 +10,8 @@ export const TrainerSchema = z.object({
       z.object({
         traineeName: z.string().min(2, "Trainee name must be at least 2 characters"),
         comment: z.string().min(1, "Comment cannot be empty"),
-        starts: z.number().min(1, "Day must be at least 1").max(5, "Day must be at most 5"),
+        rating: z.number().min(1, "Day must be at least 1").max(5, "Day must be at most 5"),
+        date: z.date()
       })
     )
     .default([])
@@ -22,7 +23,8 @@ export const CommentsSchema = z.object({
       z.object({
         traineeName: z.string().min(2, "Trainee name must be at least 2 characters"),
         comment: z.string().min(1, "Comment cannot be empty"),
-        starts: z.number().min(1, "Day must be at least 1").max(5, "Day must be at most 5"),
+        rating: z.number().min(1, "Day must be at least 1").max(5, "Day must be at most 5"),
+        date: z.date()
       })
     )
     .default([])
