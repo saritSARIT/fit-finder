2e96fbe1f2db271a83e4ba251223a2646b338502
Tamar <tamar75262@gmail.com>
2025-11-27 11:52:11 +0200

    send request

--- src/lib/validation/Training.js
+++ src/lib/validation/Training.js
@@ -1,23 +1,5 @@
 import { z } from "zod";
 
-export const ActiveTrainingSchema = z.object({
-  status: z.enum(["sent", "approved", "rejected"], "status must be 'sent', 'approved' or 'rejected'"),
-  date: z
-    .string()
-    .refine((val) => !isNaN(Date.parse(val)), "Invalid date format"),
-  traineeId: z
-    .array(z.string().min(1, "Trainee ID cannot be empty"))
-    .min(1, "At least one trainee is required"),
-
-
-  from: z.string().min(1, "Start time is required"),
-  to: z.string().min(1, "End time is required"),
-  trainerId: z.string().min(1, "Trainer ID is required"),
-  type: z.string().min(1, "Type is required"),
-  classType: z.enum(["personal", "group"], "classType must be 'personal' or 'group'"),
-
-});
-
 export const TrainingSchema = z.object({
   day: z.number(),
 
@@ -26,4 +8,14 @@ export const TrainingSchema = z.object({
   trainerId: z.string().min(1, "Trainer ID is required"),
   type: z.string(),
   classType: z.enum(["personal", "group"], "classType must be 'personal' or 'group'"),
+  status: z.enum(["empty", "sent", "approved", "rejected"], "status must be 'sent', 'approved' or 'rejected'"),
+
+  date: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), "Invalid date format")
+    .optional(),
+  traineeId: z
+    .array(z.string().min(1, "Trainee ID cannot be empty"))
+    .min(1, "At least one trainee is required")
+    .optional(),
 });
