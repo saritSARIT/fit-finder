6893b3566cff8e6697a704c0d89b98cde14a0bdc
SARITsarit <876sarit@gmail.com>
2025-12-09 10:22:39 +0200

    up

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,37 @@
 import styles from './footer.module.css';
 import logo from '../../../public/images/ff-white.png';
 
+import { 
+  FaFacebook, 
+  FaInstagram, 
+  FaLinkedin,
+  FaYoutube,
+  FaTiktok
+} from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      
       <p>תנאי שימוש</p>
-      <img src={logo.src} alt="Logo" className={styles.logo} width={70} style={{opacity: 0.9}} />
+
+      {/* אייקונים במרכז */}
+      <div className={styles.social}>
+        <a href="https://facebook.com" target="_blank"><FaFacebook /></a>
+        <a href="https://instagram.com" target="_blank"><FaInstagram /></a>
+        <a href="https://linkedin.com" target="_blank"><FaLinkedin /></a>
+        <a href="https://youtube.com" target="_blank"><FaYoutube /></a>
+        <a href="https://tiktok.com" target="_blank"><FaTiktok /></a>
+      </div>
+
+      {/* לוגו בצד */}
+      <img 
+        src={logo.src} 
+        alt="Logo" 
+        width={80} 
+        style={{ opacity: 0.9 }} 
+      />
+
     </footer>
   );
 }
